839dba8b1ca37306402f8e36dbb43051eb94dcae
Nathan Ribeiro <nathan.f.ribeiro@live.com>
2019-11-04 22:25:11 -0300

    chore: Checkin validation

--- backend/src/app/controllers/CheckinController.js
+++ backend/src/app/controllers/CheckinController.js
@@ -1,3 +1,6 @@
+import { Op } from 'sequelize';
+import { subDays, parseISO } from 'date-fns';
+
 import Checkin from '../models/Checkin';
 import Student from '../models/Student';
 
@@ -28,6 +31,23 @@ class CheckinController {
 
     if (!studentExist) {
       return res.status(400).json({ error: 'Student not found' });
+    }
+
+    const startWeek = subDays(new Date(), 7);
+
+    const amountCheckins = await Checkin.findAndCountAll({
+      where: {
+        student_id,
+        createdAt: {
+          [Op.gte]: startWeek,
+        },
+      },
+    });
+
+    if (amountCheckins.count >= 5) {
+      return res
+        .status(400)
+        .json({ error: 'You can check in only 5 times within 7 days' });
     }
 
     const { id, createdAt } = await Checkin.create({
