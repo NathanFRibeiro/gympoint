1994ee02f542c1a0e3714b10e0ce7907515481b3
Nathan Ribeiro <nathan.f.ribeiro@live.com>
2019-12-19 22:46:46 -0300

    feat: check-in features

--- mobile/src/pages/Checkin/index.js
+++ mobile/src/pages/Checkin/index.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Alert } from 'react-native';
+
+import { parseISO, formatRelative } from 'date-fns';
 
 import {
   Container,
@@ -7,32 +11,80 @@ import {
   Title,
   OccurredAt,
 } from './styles';
-import Button from '~/components/Button';
 
-const checkins = [
-  { id: '1', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '2', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '3', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '4', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '5', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '6', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '7', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '8', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '9', title: 'Checkin number #1', date: 'Today at 9' },
-  { id: '10', title: 'Checkin number #1', date: 'Today at 9' },
-];
+import Button from '~/components/Button';
+import api from '~/services/api';
 
 export default function Checkin() {
+  const userID = useSelector(state => state.auth.id);
+  const [checkins, setCheckins] = useState([]);
+
+  async function configureData(data) {
+    const checkinList = await data.reverse().map((checkin, idx) => ({
+      ...checkin,
+      title: `Check-In #${idx + 1}`,
+      dateFormatted: formatRelative(parseISO(checkin.created_at), new Date()),
+    }));
+
+    setCheckins(checkinList.reverse());
+  }
+
+  async function getCheckins() {
+    await api
+      .get(`/students/${userID}/checkins`)
+      .then(response => {
+        configureData(response.data);
+      })
+      .catch(error => {
+        const message = error.response
+          ? {
+              title: 'Could not get checkins!',
+              subtitle: error.response.data.error,
+            }
+          : {
+              title: 'Network error!',
+              subtitle: 'Check your connection.',
+            };
+
+        Alert.alert(message.title, message.subtitle);
+      });
+  }
+
+  useEffect(() => {
+    getCheckins();
+  }, []);
+
+  async function handleCheckin() {
+    await api
+      .post(`/students/${userID}/checkins`)
+      .then(async response => {
+        await getCheckins(response.data);
+      })
+      .catch(error => {
+        const message = error.response
+          ? {
+              title: 'Check-in error!',
+              subtitle: error.response.data.error,
+            }
+          : {
+              title: 'Network error!',
+              subtitle: 'Check your connection.',
+            };
+
+        Alert.alert(message.title, message.subtitle);
+      });
+  }
+
   return (
     <Container>
-      <Button>New Check-In</Button>
+      <Button onPress={() => handleCheckin()}>New Check-In</Button>
       <CheckinList
         data={checkins}
         keyExtractor={checkin => checkin.id}
         renderItem={({ item }) => (
           <CheckinItem key={item.id}>
             <Title>{item.title}</Title>
-            <OccurredAt>{item.date}</OccurredAt>
+            <OccurredAt>{item.dateFormatted}</OccurredAt>
           </CheckinItem>
         )}
       />
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.35:3334/',
+  baseURL: 'http://192.168.1.36:3334/',
 });
 
 export default api;
